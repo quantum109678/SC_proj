dfffb4bbd6607722db5d6b8e713c1e11fb7e160a
quantum109678 <akkashyap8991@gmail.com>
2018-10-31 01:08:20 +0530

    Trial runs

--- Data/p1.py
+++ Data/p1.py
@@ -1,8 +1,9 @@
 import pandas as pd 
 import numpy as np 
 import datetime
+import csv
 
-data1=pd.read_csv('data1.csv')
+data1=pd.read_csv('new_sensor.csv')
 des1=data1.describe()
 
 
@@ -16,33 +17,38 @@ diff_lon=max_lon - min_lon
 
 dlat=diff_lat/100
 dlon=diff_lon/100
-print(dlat,dlon)
-print(des1)
-tar=data1['date'][3]
-tar=tar.split()
-print(tar)
-currt=datetime.datetime.strptime(tar[1],"%H:%M:%S")
-st_h=currt.hour
-values=0
-count=0
-st_lat=min_lat+dlat*50
-end_lat=min_lat+dlat*51
-st_lon=min_lon+dlon*50
-end_lon=min_lon+dlon*51
-print(type(st_lon))
-print(st_lat,end_lat,st_lon,end_lon)
+
+st_h=8
+
+print(st_h)
+reord8=[[0 for x in range(100)] for y in range(100)]
+counts=[[0 for x in range(100)] for y in range(100)]
+		
 for i in range(data1.shape[0]):
-	#print(i)
-	if(data1['latitude'][i] <= 50 and data1['latitude'][i]>=0):
-	#if(data1['latitude'][i] >= st_lat and data1['latitude'][i] < end_lat):
-		#if(data1['longitude'][i] >= st_lon and data1['longitude'][i] < end_lon):
-		print(i,data1['latitude'][i])
-		#curr=data1['date'][i]
-		#curr=curr.split()
-		#currt=datetime.datetime.strptime(curr[1],"%H:%M:%S")
-		#if(currt.hour==st_h):
-		#	values+=data1['co'][i]
-		#	count+=1
-if(count!=0):
-	print(values/count,count)
+	print(i)
+	curr_lat=data1['latitude'][i]
+	curr_lon=data1['longitude'][i]
+	row_num=int((curr_lat - min_lat)/dlat)
+	col_num=int((curr_lon - min_lon)/dlat)
+	print(row_num,col_num)
+
+	
+	curr=data1['date'][i]
+	curr=curr.split()
+	currt=datetime.datetime.strptime(curr[1],"%H:%M:%S")
+					#print(currt)
+	if(currt.hour==st_h):
+		reord8[row_num][col_num]+=data1['co'][i]
+		counts[row_num][col_num]+=1
+
+for x in  range(100):
+	for y in range(100):
+		if counts[x][y]!=0:
+			reord8[x][y]=reord8[x][y]/counts[x][y]
+
+
+with open("8am.csv","w+") as my_csv:
+    csvWriter = csv.writer(my_csv,delimiter=',')
+    csvWriter.writerows(reord8)						#print(data1['co'][i])
+
 
